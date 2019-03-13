2f10418236a2b5869a802fdff15d8bf98133866e
zeley93 <becquart.quentin@gmail.com>
2019-03-13 02:28:02 -0700

    fixing ignore of files

--- client/src/store/reducers/auth.js
+++ client/src/store/reducers/auth.js
@@ -6,8 +6,12 @@ const DEFAULT_STATE = {
 }
 
 export default (state = DEFAULT_STATE, action) => {
-    switch(){
-        case:
+    switch(action.type){
+        case SET_CURRENT_USER:
+            return {
+                isAuthenticated:!!Object.keys(action.user).length,
+                user: action.user
+            };
 
         default:
             return state;
