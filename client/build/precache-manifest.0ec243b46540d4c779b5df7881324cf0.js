6a5a222154b9a21246088648b5f4af645eecaf02
zeley93 <becquart.quentin@gmail.com>
2019-07-30 15:21:50 +0000

    fixed error of production build
    	new file:   client/build/asset-manifest.json
    	new file:   client/build/index.html
    	new file:   client/build/precache-manifest.0ec243b46540d4c779b5df7881324cf0.js
    	new file:   client/build/service-worker.js
    	new file:   client/build/static/css/2.0989d313.chunk.css
    	new file:   client/build/static/css/2.0989d313.chunk.css.map
    	new file:   client/build/static/css/main.4addf7bd.chunk.css
    	new file:   client/build/static/css/main.4addf7bd.chunk.css.map
    	new file:   client/build/static/js/2.4f5b0c1b.chunk.js
    	new file:   client/build/static/js/2.4f5b0c1b.chunk.js.map
    	new file:   client/build/static/js/main.863343c6.chunk.js
    	new file:   client/build/static/js/main.863343c6.chunk.js.map
    	new file:   client/build/static/js/runtime~main.fdfcfda2.js
    	new file:   client/build/static/js/runtime~main.fdfcfda2.js.map
    	modified:   client/package-lock.json
    	modified:   client/src/services/api.js
    	new file:   reactClient.service
    	modified:   server/package-lock.json

---
+++ client/build/precache-manifest.0ec243b46540d4c779b5df7881324cf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "863343c66fc1d3c87d94",
+    "url": "/static/js/main.863343c6.chunk.js"
+  },
+  {
+    "revision": "4f5b0c1b4a4bd0d9fb61",
+    "url": "/static/js/2.4f5b0c1b.chunk.js"
+  },
+  {
+    "revision": "863343c66fc1d3c87d94",
+    "url": "/static/css/main.4addf7bd.chunk.css"
+  },
+  {
+    "revision": "4f5b0c1b4a4bd0d9fb61",
+    "url": "/static/css/2.0989d313.chunk.css"
+  },
+  {
+    "revision": "84d22eecab4144a777c19df0ffbd1d92",
+    "url": "/index.html"
+  }
+];
