96ab6ac6c307bf724b2e3a3842defe6b804e8f68
aladeen <becquart.quentin@gmail.com>
2019-08-01 06:30:09 -0700

    tring to fix invariant violation

--- client/src/components/ErrorMessage.jsx
+++ client/src/components/ErrorMessage.jsx
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 
 const ErrorMessage = ({ error }) => (
-    <Fragment> { error.message && <div className="error" >{error.message}</div>} </Fragment>
+    <Fragment> { error && <div className="error" >{error}</div>} </Fragment>
 );
 
 export default connect(store => ({ error: store.error }))(ErrorMessage);
